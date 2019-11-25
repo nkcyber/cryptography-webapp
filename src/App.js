1733d5b46756a87ac53207d9476521f29bb61ba9
John Arlinghaus <18181026+JArlinghaus@users.noreply.github.com>
2019-11-25 00:18:39 -0500

    Added in placeholder routes and components for two cophers

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
 import React, { Component } from 'react';
 import {  withRouter,
+          Switch,
           Redirect,
           Route } from "react-router";
 import { withLayout } from "./layout"
+import GenericCipher from "./GenericCipher"
+import { produce } from 'immer';
 import logo from './logo.svg';
 import './App.css';
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      buffers: {'global.encryption': ""},
+      options: {},
+    };
+    this.onBufferUpdate = this.onBufferUpdate.bind(this);
+  }
+
+  onBufferUpdate(bufferName, contents, global=false) {
+    this.setState(produce((draft) => {
+      if (global) {
+        draft.buffers[`global.${bufferName}`] = contents
+      } else {
+        draft.buffers[`${this.props.location.pathname}.${bufferName}`] = contents
+      }
+    }));
+  }
+
   render() {
     return (
       <div className="App">
         <Route exact path="/">
           <Redirect to="/caesar-cipher"/>
         </Route>
+        <Switch>
+          <Route path="/caesar-cipher">
+            <GenericCipher
+              buffers={this.state.buffers}
+              options={this.state.options}
+              onBufferUpdate={this.onBufferUpdate}
+              encryptionFn={(str) => str.toUpperCase()}
+              decryptionFn={(str) => str.toLowerCase()}
+            />
+          </Route>
+          <Route path="/frequency-analysis">
+            <p></p>
+          </Route>
+          <Route path="/multiplicative-cipher">
+            <GenericCipher
+              buffers={this.state.buffers}
+              options={this.state.options}
+              onBufferUpdate={this.onBufferUpdate}
+              encryptionFn={(str) => str.toUpperCase()}
+              decryptionFn={(str) => str.toLowerCase()}
+            />
+          </Route>
+        </Switch>
       </div>
     );
   }
