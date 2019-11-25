7b5c359ae5cd383806604749e22fd994c89a2c39
John Arlinghaus <18181026+JArlinghaus@users.noreply.github.com>
2019-11-24 21:42:19 -0500

    Created basic sidebar component for navigating the website

---
+++ src/SideBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  Route,
+  Link,
+} from "react-router-dom";
+
+
+export function SideBar() {
+  return (
+    <>
+    <h4>Ciphers/Tools</h4>
+    <ul class="list-group">
+      <li class="list-group-item">
+        <Link to="/frequency-analysis">Frequency Analysis</Link>
+      </li>
+      <li class="list-group-item">
+        <Link to="/ceasar-cipher">Ceaser Cipher</Link>
+      </li>
+      <li class="list-group-item">
+        <Link to="/multiplicative-cipher">Multiplicative</Link>
+      </li>
+    </ul>
+    </>
+  )
+}
