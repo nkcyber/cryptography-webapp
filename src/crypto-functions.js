e307a048b49e84b70b69607a1379619e41a0ca41
John Arlinghaus <18181026+JArlinghaus@users.noreply.github.com>
2019-11-27 22:31:44 -0500

    Added function to format strings into blocks

--- src/crypto-functions.js
+++ src/crypto-functions.js
@@ -3,3 +3,7 @@
 export function removePunctuation(str) {
   return str.replace(/[^a-zA-Z]+/g, '')
 }
+
+export function blockFormat(str, len=5) {
+  return removePunctuation(str).match(new RegExp('.{1,' + len + '}', 'g')).reduce((a, b) => a + " " + b);
+}
--- src/crypto-functions.test.js
+++ src/crypto-functions.test.js
@@ -4,3 +4,11 @@ import * as crypto from './crypto-functions.js'
 it('removes punctuation', () => {
   expect(crypto.removePunctuation("test..2341&*")).toEqual('test')
 })
+
+it('formats in blocks of length 5 by defualt', () => {
+  expect(crypto.blockFormat("testing the formatting")).toEqual('testi ngthe forma tting')
+})
+
+it('formats in blocks of length given by arguement', () => {
+  expect(crypto.blockFormat("testing the formatting", 2)).toEqual('te st in gt he fo rm at ti ng')
+})
