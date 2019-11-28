84c91438e0d6bc9c2f10e1bd108e29f3dc7c68aa
John Arlinghaus <18181026+JArlinghaus@users.noreply.github.com>
2019-11-27 23:14:42 -0500

    Fixed failing tests

--- src/crypto-functions.test.js
+++ src/crypto-functions.test.js
@@ -2,15 +2,15 @@ import * as crypto from './crypto-functions.js'
 
 
 it('removes punctuation', () => {
-  expect(crypto.cleanupString("test..2341&*")).toEqual('test')
+  expect(crypto.cleanupString("test..2341&*")).toEqual('TEST')
 })
 
 it('formats in blocks of length 5 by defualt', () => {
-  expect(crypto.blockFormat("testing the formatting")).toEqual('testi ngthe forma tting')
+  expect(crypto.blockFormat("testing the formatting")).toEqual('TESTI NGTHE FORMA TTING')
 })
 
 it('formats in blocks of length given by arguement', () => {
-  expect(crypto.blockFormat("testing the formatting", 2)).toEqual('te st in gt he fo rm at ti ng')
+  expect(crypto.blockFormat("testing the formatting", 2)).toEqual('TE ST IN GT HE FO RM AT TI NG')
 })
 
 it('correctly enciphers', () => {
