4fd12603560a747b9c016758860e204e463d45a0
John Arlinghaus <18181026+JArlinghaus@users.noreply.github.com>
2019-11-28 22:36:01 -0500

    Renamed class attributes to className

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -9,20 +9,20 @@ export function SideBar() {
   return (
     <>
     <h4>Ciphers/Tools</h4>
-    <ul class="list-group">
-      <li class="list-group-item">
+    <ul className="list-group">
+      <li className="list-group-item">
         <Link to="/frequency-analysis">Frequency Analysis</Link>
       </li>
-      <li class="list-group-item">
+      <li className="list-group-item">
         <Link to="/masc">MASC</Link>
       </li>
-      <li class="list-group-item">
+      <li className="list-group-item">
         <Link to="/caesar-cipher">Caesar Cipher</Link>
       </li>
-      <li class="list-group-item">
+      <li className="list-group-item">
         <Link to="/multiplicative-cipher">Multiplicative</Link>
       </li>
-      <li class="list-group-item">
+      <li className="list-group-item">
         <Link to="/affine-cipher">Affine</Link>
       </li>
     </ul>
